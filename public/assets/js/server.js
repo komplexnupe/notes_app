728f0f1d1c02eb011c05bddc543d73b9c033fb87
Komplexnupe <leonard.love87@icloud.com>
2020-09-01 22:09:59 -0400

    initial setup

---
+++ public/assets/js/server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 2020;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Notes (DATA)
+// =============================================================
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../../index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "../../notes.html"));
+});
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on: http://localhost:" + PORT);
+  });
+  
